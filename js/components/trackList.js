3328269f432bd34f0b5cb04e2f33a257daadfa6c
Maady <vlad030911@gmail.com>
2023-08-31 09:58:57 +0300

    commit

--- js/components/trackList.js
+++ js/components/trackList.js
@@ -54,62 +54,7 @@ export const music_list = [
         name : 'Storage',
         music : 'mp3/@maady - Vtorogo 71 Bpmflp.mp3'
     },
-    {
-        img : 'img/cover_beats/1.jpg',
-        name : 'Stay',
-        music : 'mp3/Летний хит.mp3'
-    },
-    {
-        img : 'img/cover_beats/2.jpg',
-        name : 'Falling',
-        music : 'mp3/DripDj.mp3'
-    },
-    {
-        img : 'img/cover_beats/3.jpg',
-        name : 'Faded',
-        music : 'mp3/undefinded.mp3'
-    },
-    {
-        img : 'img/cover_beats/4.jpg',
-        name : 'Rather',
-        music : 'mp3/Закольцовка.mp3'
-    },
-    {
-        img : 'img/cover_beats/5.jpg',
-        name : 'XXX',
-        music : 'mp3/Вместо икса.mp3'
-    },
-    {
-        img : 'img/cover_beats/6.jpg',
-        name : 'Shell',
-        music : 'mp3/@maady -  ChillDrip 180 BPM.mp3'
-    },
-    {
-        img : 'img/cover_beats/7.jpg',
-        name : 'Chill',
-        music : 'mp3/@maady -  ChillSound 170 BPM.wav'
-    },
-    {
-        img : 'img/cover_beats/8.jpg',
-        name : 'Droe',
-        music : 'mp3/@maady -  Droe 157 BPM.mp3'
-    },
-    {
-        img : 'img/cover_beats/9.jpg',
-        name : 'Hate you',
-        music : 'mp3/Hate you.mp3'
-    },
-    {
-        img : 'img/cover_beats/10.jpg',
-        name : 'Smile piano',
-        music : 'mp3/Smile piano.mp3'
-    },
-    {
-        img : 'img/cover_beats/11.jpg',
-        name : 'Storage',
-        music : 'mp3/@maady - Vtorogo 71 Bpmflp.mp3'
-    },
-    
+
     
     
 ];
