6d150ed500bf34e05ae9562dd47ad017d0dea2d3
Maady <vlad030911@gmail.com>
2023-09-04 13:36:45 +0300

    stop

--- js/components/mobile/swips.js
+++ js/components/mobile/swips.js
@@ -1,11 +1,12 @@
 import Swiper from 'swiper/bundle';
 
 export function swipToTracks (){
-    const swiper = new Swiper('.swiper', {
+    return swiper = new Swiper('.swiper', {
 
         pagination: {
           el: '.swiper-pagination',
         },
+        
 
         autoHeight: true,
         effect: 'fade',
